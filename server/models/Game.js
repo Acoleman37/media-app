8d8a5844e4eb13d2ed408c46fa32dce699df81a7
NickNocc <haretakis4@gmail.com>
2022-03-23 13:29:43 -0700

    Game model created

--- server/models/Game.js
+++ server/models/Game.js
@@ -2,6 +2,22 @@ const { Schema, model } = require('mongoose');
 
 const gameSchema = new Schema(
     {
-        
+        gameTitle: {
+            type: String,
+            require: true,
+        },
+        genre: {
+            type: String
+        },
+        achievements: {
+            type: String
+        },
+        progress: {
+            type: String
+        }
     }
-)
+);
+
+const Game = model('Game', gameSchema);
+
+module.exports = Game;
