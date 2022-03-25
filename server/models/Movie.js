1bcc28cac8d6abae5a6163e715000dabab36b45b
Acoleman37 <aleccoleman123@yahoo.com>
2022-03-24 20:36:41 -0700

    .

---
+++ server/models/Movie.js
@@ -0,0 +1,25 @@
+const { Schema } = require('mongoose');
+
+const movieSchema = new Schema
+(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        genre: [{
+            type: String
+        }],
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+        },
+    }
+);
+
+const Movie = model('Movie', movieSchema);
+
+module.export = Movie;
