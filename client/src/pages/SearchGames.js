abc9ea4177c14879b51d76a29f8397dce9b0f2c7
NickNocc <haretakis4@gmail.com>
2022-03-29 19:47:53 -0700

    formatted page

--- client/src/pages/SearchGames.js
+++ client/src/pages/SearchGames.js
@@ -1,76 +1,79 @@
-import { useState, useEffect } from 'react';
-import Auth from '../utils/auth';
-import { searchGameAPI } from '../utils/steam.js';
-import { saveGameIds, getSavedGameIds, saveMovieIds } from '../utils/localStorage';
-import { SAVE_GAME } from '../utils/mutations';
-import { useMutation } from '@apollo/client'
+import { useState, useEffect } from "react";
+import Auth from "../utils/auth";
+import { searchGameAPI } from "../utils/steam.js";
+import {
+  saveGameIds,
+  getSavedGameIds,
+  saveMovieIds,
+} from "../utils/localStorage";
+import { SAVE_GAME } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
 
 const SearchGames = () => {
+  const [searchedGames, setSearchedGames] = useState([]);
 
-    const [searchedGames, setSearchedGames] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-    const [searchInput, setSearchInput] = useState('');
+  const [savedGameIds, setSavedGameIds] = useState(getSavedGameIds());
 
-    const [savedGameIds, setSavedGameIds] = useState(getSavedGameIds());
+  const [saveGame] = useMutation(SAVE_GAME);
 
-    const [saveGame] = useMutation(SAVE_GAME);
+  useEffect(() => {
+    return () => saveGameIds(savedGameIds);
+  });
 
-    useEffect(() => {
-        return () => saveGameIds(savedGameIds);
-    });
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+    if (!searchInput) {
+      return false;
+    }
 
-        if(!searchInput) {
-            return false;
-        }
+    try {
+      const response = await searchGameAPI(searchInput);
 
-        try {
-            const response = await searchGameAPI(searchInput);
+      if (response.ok) {
+        throw new Error("something went wrong!");
+      }
 
-            if (response.ok) {
-                throw new Error('something went wrong!');
-            }
+      const { items } = await response.json();
 
-            const { items } = await response.json();
+      const gameData = items.map((game) => ({
+        gameTitle: game.name,
+        genre: game.genres[0],
+        achievements: game.playtime,
+        progress: "",
+        image: game.background_image || "",
+      }));
 
-            const gameData = items.map((game) => ({
-                gameTitle: game.name,
-                genre: game.genres[0],
-                achievements: game.playtime,
-                progress: '',
-                image: game.background_image || '',
-            }));
+      setSearchedGames(gameData);
+      setSearchInput("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-            setSearchedGames(gameData);
-            setSearchInput('');
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  const handleSaveGame = async (gameId) => {
+    const gameToSave = searchedGames.find((game) => game.gameId === gameId);
 
-    const handleSaveGame = async (gameId) => {
-        const gameToSave = searchedGames.find((game) => game.gameId === gameId);
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
 
-        if (!token) {
-            return false;
-        }
+    try {
+      await saveMovieIds({
+        variables: { ...gameToSave },
+      });
 
-        try {
-            await saveMovieIds({
-                variables: { ...gameToSave }
-            })
+      setSavedGameIds([...savedGameIds, gameToSave.gameId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-            setSavedGameIds([...savedGameIds, gameToSave.gameId]);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  return "JSX GOES HERE";
+};
 
-    return ('JSX GOES HERE')
-}
-
-export default SearchGames
+export default SearchGames;
