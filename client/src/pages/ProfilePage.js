2a89a521a589daa5b3eefc3c2a74de3600da9175
Spotts91 <oakshassa864@gmail.com>
2022-03-30 16:10:35 -0700

    app.js changes

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Grid, Image } from 'semantic-ui-react';
 
 class Carousel extends Component {
