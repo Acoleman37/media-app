3889cd5a81f5e631214bdde1667aa0bcd63582b2
Spotts91 <oakshassa864@gmail.com>
2022-03-31 15:46:51 -0700

    dependency tweaks

--- client/src/pages/LibraryPage.js
+++ client/src/pages/LibraryPage.js
@@ -3,7 +3,6 @@ import { Grid, Image } from 'semantic-ui-react';
 import {useQuery} from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
 
-<<<<<<< HEAD
 
 
 const GridCelled = () => {
@@ -14,10 +13,6 @@ const GridCelled = () => {
   console.log(userData);
 
 return (
-=======
-const GridCelled = () => (
-  // need to be mapped with api call
->>>>>>> 78624984bf1349621834969f27220b5c106c1213
   <Grid celled>
     <Grid.Row>
       <Grid.Column width={3}>
