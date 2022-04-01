b3e08e04d9ee4b31451262129b827e2fe9c79354
NickNocc <haretakis4@gmail.com>
2022-03-31 17:55:56 -0700

    Update localStorage.js

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -28,3 +28,33 @@ export const removeMovieId = (movieId) => {
 
   return true;
 };
+
+export const getSavedGameIds = () => {
+  const savedGameIds = localStorage.getItem('saved_games')
+  ? JSON.parse(localStorage.getItem('saved_games'))
+  : [];
+
+  return savedGameIds
+};
+
+export const saveGameIds = (gameIdArr) => {
+  if (gameIdArr.length) {
+    localStorage.setItem('saved_games', JSON.stringify(gameIdArr));
+  } else {
+    localStorage.removeItem('saved_games');
+  }
+};
+
+export const removeGameId = (gameId) => {
+  const savedGameIds = localStorage.getItem('saved_games')
+    ? JSON.parse(localStorage.getItem('saved_games'))
+    : null;
+
+  if (!savedGameIds) {
+    return false;
+  }
+  const updatedSavedGameIds = savedGameIds?.filter((savedMovieId) => savedMovieId !== gameId);
+  localStorage.setItem('saved_games', JSON.stringify(updatedSavedGameIds));
+
+  return true;
+};
