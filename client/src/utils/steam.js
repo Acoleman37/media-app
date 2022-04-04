717ac08b4c8372f2d6836ad17bbe1d8c9fbcdb87
NickNocc <haretakis4@gmail.com>
2022-04-04 14:09:06 -0700

    Update steam.js

--- client/src/utils/steam.js
+++ client/src/utils/steam.js
@@ -2,28 +2,19 @@ var axios = require('axios').default
 
 export const searchGameAPI = async (query) => {
 
-  return fetch(`https://api.rawg.io/api/games?key=44ea66fccf67441c938faa074f82e40c&search=${query}`)
-    .then(function (response) {
-      console.log(response.data);
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-    // var options = {
-        // method: "GET",
-        // url: `https://api.rawg.io/api/games?key=44ea66fccf67441c938faa074f82e40c&search=sonic`,
-      // };
+    var options = {
+        method: "GET",
+        url: `https://api.rawg.io/api/games?key=44ea66fccf67441c938faa074f82e40c&search=${query}`,
+      };
     
-      // axios
-        // .request(options)
-        // .then(function (response) {
-          // console.log(response.data);
-        // })
-        // .catch(function (error) {
-          // console.error(error);
-        // });
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
 };
 
 
