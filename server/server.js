faaf2415d1a135e15730eab49d854bf4f7044303
NickNocc <haretakis4@gmail.com>
2022-06-09 22:17:48 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,3 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, '../client/build')));
+}
+// app.use(routes);
