b27c91d0c22a2d7be0272ec5c6f1b1b3f3a4514e
NickNocc <haretakis4@gmail.com>
2022-06-09 22:18:17 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,3 +32,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) => {
 res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
